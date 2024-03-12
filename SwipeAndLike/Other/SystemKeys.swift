456d7a6da3c07b6f2585c77b1992275518b9ed1d
Marco Zamora <work.marcozamora@gmail.com>
2024-03-12 16:30:07 -0500

    * small changes post merge conflict

---
+++ SwipeAndLike/Other/SystemKeys.swift
@@ -0,0 +1,12 @@
+//
+//  SystemKeys.swift
+//  SwipeAndLike
+//
+//  Created by Marco Zamora on 1/18/24.
+//
+
+import Foundation
+
+enum SystemKeys: String {
+    case apiKey = "d57d056e60mshab91d390c83e199p1dd2d6jsnd48b7023f960"
+}
